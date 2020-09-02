6eb096dea52cc57eec7d94daf1bf7ce84a32c19f
worawitboat <worawit.pa.61@ubu.ac.th>
2020-09-02 10:32:16 +0700

    Updates

---
+++ precache-manifest.3247a89b011d555677c215fe3598eb04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5586b4621a34aedc1daa782d155edda6",
+    "url": "/tic-tac-toe.github.io/index.html"
+  },
+  {
+    "revision": "0a0633dbac69eacf6477",
+    "url": "/tic-tac-toe.github.io/static/css/main.a1ea7abd.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/tic-tac-toe.github.io/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe.github.io/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a0633dbac69eacf6477",
+    "url": "/tic-tac-toe.github.io/static/js/main.ec442494.chunk.js"
+  },
+  {
+    "revision": "78d662f9c1538a8c5b06",
+    "url": "/tic-tac-toe.github.io/static/js/runtime-main.1c7e0e77.js"
+  }
+]);
